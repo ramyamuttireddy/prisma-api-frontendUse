0ba47a45ecc61c99e4de2b670c5b1ecc75e90aad
Ramya <muttireddyramya9@gmail.com>
2025-01-07 15:16:51 +0530

    commit

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -112,7 +112,7 @@ const Home = () => {
                   src={product.image}
                   alt={product.name}
                   className="w-full h-60 object-cover"
-                  loading="lazy" // Lazy loading added here
+                  loading="lazy"
                 />
               </Link>
               <div className="p-4">
